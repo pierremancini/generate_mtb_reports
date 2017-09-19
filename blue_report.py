aae05bf041bbe6fdda7935d4dbb1722056e8da97
Pierre Mancini <pierremncini@gmail.com>
2017-09-19 15:02:01 +0200

    Implémentation de l'envoi du fichier.

--- blue_report.py
+++ blue_report.py
@@ -6,8 +6,7 @@
 Module call report.py (python3)
 """
 
-from flask import Blueprint, render_template, abort
-from jinja2 import TemplateNotFound
+from flask import Blueprint, send_file
 
 
 get_report_page = Blueprint('get_report_page', __name__)
@@ -15,4 +14,7 @@ get_report_page = Blueprint('get_report_page', __name__)
 
 @get_report_page.route('/')
 def get_report():
-    return 'Test'
+
+    return send_file('data/T02-0002-DX-001O.pdf', mimetype='application/pdf')
+
+
