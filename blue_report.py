13a22e55a982a25fe735cc09b7507eecc32eb8c3
Pierre Mancini <pierremncini@gmail.com>
2017-09-26 14:33:41 +0200

    Appel du script report.py

--- blue_report.py
+++ blue_report.py
@@ -3,18 +3,36 @@
 
 
 """
-Module call report.py (python3)
+    Display pre-MTB pdf file.
 """
 
-from flask import Blueprint, send_file
+from flask import Blueprint, send_file, request
+
+import flask.json
+
+import subprocess
+import shlex
 
 
 get_report_page = Blueprint('get_report_page', __name__)
 
 
-@get_report_page.route('/')
+def call_cmd(cmd):
+    """Call system command."""
+
+    args = shlex.split(cmd)
+    process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        universal_newlines=True, shell=False)
+    stdout, stderr = process.communicate()
+    print(stderr)
+    print(stdout)
+
+
+@get_report_page.route('/', methods=['GET'])
 def get_report():
 
-    return send_file('data/T02-0002-DX-001O.pdf', mimetype='application/pdf')
+    patient_id = request.args.get("patient_id")
 
+    call_cmd('python3 report.py -id {}'.format(patient_id))
 
+    return send_file('data/{}.pdf'.format(patient_id), mimetype='application/pdf')
